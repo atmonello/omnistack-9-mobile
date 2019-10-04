2429f405739094779605959f6e4ba34597664fb9
Andre Monello <atmonello@gmail.com>
2019-10-03 21:00:54 -0300

    Commit #5
    - Iniciando página de Lista

--- src/pages/List.js
+++ src/pages/List.js
@@ -1,6 +1,33 @@
-import React from 'react';
-import { View } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { SafeAreaView, View, Text, AsyncStorage, Image, StyleSheet } from 'react-native';
+
+import logo from '../assets/logo.png';
 
 export default function List() {
-  return <View />
+  const [techs, setTechs] = useState([]);
+
+  useEffect(() => {
+    AsyncStorage.getItem('techs').then(storedTechs => {
+      const techsArray = storedTechs.split(',').map(tech => tech.trim());
+      setTechs(techsArray);
+    });
+  }, []);
+  return (
+    <SafeAreaView style={styles.container}>
+      <Image source={logo} style={styles.logo} />
+      <Text>{techs}</Text>
+    </SafeAreaView>
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1
+  },
+  logo: {
+    height: 32,
+    resizeMode: 'contain',
+    alignSelf: 'center',
+    marginTop: 10
+  }
+})
